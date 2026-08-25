4affd9d6a720dc3137828e196405d6c3d87f2e09
ovieadanna26-code <ovie.adanna26@gmail.com>
2026-08-25 18:24:22 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,3 +4,5 @@ window.SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
 window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_PUBLISHABLE_KEY';
 
 // Never put a Supabase service_role key in this file or in GitHub Pages.
+https://olobpbwpnlfgitpgrydu.supabase.co
+sb_publishable_nZyerthZd3VFSIMtHPe12A_K25d_gq2
